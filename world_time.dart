df1afecd0d9a6ded9a23b98a11f1d1883b4dd926
Ananya-Kannan <91649989+Ananya-Kannan@users.noreply.github.com>
2022-05-06 00:40:34 +0530

    Add files via upload

---
+++ world_time.dart
@@ -0,0 +1,39 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:intl/intl.dart';
+
+class WorldTime {
+
+  String location; // location name for UI
+  String time = ""; // the time in that location
+  String flag; // url to an asset flag icon
+  String url;// location url for api endpoint
+  late bool isDayTime;
+
+  WorldTime({ required this.location, required this.flag, required this.url });
+
+  Future<void> getTime() async {
+    Uri url = Uri.parse('http://worldtimeapi.org/api/timezone/${this.url}');
+    Response response = await get(url);
+
+    if (response.statusCode == 200) {
+      try {
+        Map data = jsonDecode(response.body);
+
+        String datetime = data['datetime'];
+        String offset = data['utc_offset'].substring(1, 3);
+
+        DateTime now = DateTime.parse(datetime);
+        now = now.add(Duration(hours: int.parse(offset)));
+
+        time = DateFormat.jm().format(now);
+        isDayTime = now.hour > 6 && now.hour < 20? true : false;
+
+      } catch (e) {
+        time = 'Could not get time';
+      }
+    }
+  }
+}
+
+   // WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
